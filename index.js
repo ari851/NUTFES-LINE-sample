0638fc7ff15cd468531b66c0079b98c25ec3fd89
ari851 <125197381+ari851@users.noreply.github.com>
2023-04-03 00:55:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function onSubmit(){
-    const text = document.getElementById('answer').value;
+    const text = document.getElementById('answer','name','udeage','fukkin','haikin','sukuwat').value;
 
     const msg = "【送信内容】\n" + text;
     console.log(msg);
