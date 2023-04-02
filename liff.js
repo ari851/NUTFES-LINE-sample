48ff54868fa8494b6dfa7b3086140179ca7c5cf4
ari851 <125197381+ari851@users.noreply.github.com>
2023-04-03 07:29:04 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -47,6 +47,27 @@ function sendMessages(text) {
     });
 }
 
+        
+        const params = (new URL(document.location)).searchParams;
+        const key = params.get('key');
+
+        $(function () {
+            $('form').submit(function () {
+                const name    = $('input[name="name"]').val();
+                const udetate = $('input[name="udetate"]').val();
+                const fukkin  = $('input[name="fukkin"]').val();
+                const haikin  = $('input[name="haikin"]').val();
+                const sukuwat = $('input[name="sukuwat"]').val();
+                
+                const msg = `${name}\n${udetate}\n${fukkin}\n${haikin}\n${sukuwat}`;
+                sendText(msg);
+
+                return false;
+            });
+        });
+
+
+
 // Webブラウザからメッセージ送信
 function shareTargetPicker(text) {
     console.log('in shareTargetPicker');
