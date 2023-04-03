8022b2dcf3cc144956ea4609c036168ab838c765
ari851 <125197381+ari851@users.noreply.github.com>
2023-04-04 01:10:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ function onSubmit() {
   var haikin = document.myform.haikin.value;
   var sukuwat = document.myform.sukuwat.value;
 
-  // 入力値が空欄の場合はアラートを表示してフォームの送信を中止する
-  if (name == "" || udetate == "" || fukkin == "" || haikin == "" || sukuwat == "") {
-    alert("全ての項目を入力してください");
-    return false;
-  }
-
   // 確認のアラートを表示して、OKを選択した場合にフォームを送信する
   var message = "入力内容を確認してください\n\n名前：" + name + "\n腕立て：" + udetate + "\n腹筋：" + fukkin + "\n背筋：" + haikin + "\nスクワット：" + sukuwat;
   if (confirm(message)) {
