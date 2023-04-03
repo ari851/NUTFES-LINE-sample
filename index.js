83d830de019485514109917c4c6bd3744253e7ff
ari851 <125197381+ari851@users.noreply.github.com>
2023-04-03 09:19:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function onSubmit(){
-    const text = document.myform.value;
+        const params = (new URL(document.location)).searchParams;
+        const key = params.get('key');
 
-    const msg = "【送信内容】n\" + text;
-    console.log(msg);
-    sendText(msg);
+        $(function () {
+            $('form').submit(function () {
+                const name    = $('input[name="name"]').val();
+                const udetate = $('input[name="udetate"]').val();
+                const fukkin  = $('input[name="fukkin"]').val();
+                const haikin  = $('input[name="haikin"]').val();
+                const sukuwat = $('input[name="sukuwat"]').val();
+                
+                const msg = `【送信内容】\n${name}\n${udetate}\n${fukkin}\n${haikin}\n${sukuwat}`;
+                sendText(msg);
 
-    return false;
-}
+                return false;
+            });
+        });
